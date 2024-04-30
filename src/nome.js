6e743a41da99bc23a4479993c08cf393cbf206fa
TwuitePanda <glauco.laurent@gmail.com>
2024-04-30 19:14:24 -0300

    primeiro commit

---
+++ src/nome.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplo de uso das funções
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+const mensagemSaudacao = saudacao("Maria");
+console.log(mensagemSaudacao);
